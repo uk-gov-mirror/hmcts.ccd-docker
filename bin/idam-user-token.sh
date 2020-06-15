37cf929d39b70b71465ddb8f287f2a803a4bbe23
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-06-15 14:51:13 +0100

    ACA + PRD docker setup

--- bin/idam-user-token.sh
+++ bin/idam-user-token.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-IMPORTER_USERNAME=${1:-ccd.docker.default@hmcts.net}
-IMPORTER_PASSWORD=${2:-Pa55word11}
+IMPORTER_USERNAME=${1:-idamOwner@hmcts.net}
+IMPORTER_PASSWORD=${2:-Ref0rmIsFun}
 IDAM_URI=${IDAM_STUB_LOCALHOST:-http://localhost:5000}
 REDIRECT_URI="http://localhost:3451/oauth2redirect"
 CLIENT_ID="ccd_gateway"
 CLIENT_SECRET="ccd_gateway_secret"
 
-code=$(curl ${CURL_OPTS} -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/oauth2/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}" -d "" | jq -r .code)
+code=$(
+curl --silent --show-error -X POST "${IDAM_URI}/o/token" \
+      -H "accept: application/json" \
+      -H "Content-Type: application/x-www-form-urlencoded" \
+      --data-urlencode "client_id=${CLIENT_ID}" \
+      --data-urlencode "client_secret=${CLIENT_SECRET}" \
+      --data-urlencode "grant_type=password" \
+      --data-urlencode "username=${IMPORTER_USERNAME}" \
+      --data-urlencode "password=${IMPORTER_PASSWORD}" \
+      --data-urlencode "redirect_uri=${REDIRECT_URI}" \
+      --data-urlencode "scope=openid roles profile manage-user" | jq -r .access_token)
 
-curl ${CURL_OPTS} -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" -d "" | jq -r .access_token
+echo "${code}"
